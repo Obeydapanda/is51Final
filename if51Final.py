047354d2093744831fef8d66eea6189ed9b6e539
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-12-17 18:33:37 -0800

    Exam-End Finished

--- if51Final.py
+++ if51Final.py
@@ -1,38 +1,35 @@
-"""
+
+
 def main():
+    infile = open("final.txt", "r")
+    grades = [int(word) for word  in infile]
+    infile.close()
+
+    above_average = calculate_percent_above_avereage(grades) * 100
+
     
-    kickstart()
-    calculate_average()
-    calculate_percent_above_avereage()
 
     print("Number of Grades:", len(grades))
-    print("Average Grade:", calculate_average(grades))
-    print("Percentage of Grades Above Average:", calculate_percent_above_avereage(grades), "%")
+    print("Average Grade:", calculate_average(grades), "%")
+    print("Percentage of Grades Above Average:", round(above_average, 2), "%")
+
+
 
 def calculate_percent_above_avereage(grades):
     avg = calculate_average(grades)
     counter = 0
-    for grades in grades:
+    for grade in grades:
         if grade > avg:
             counter += 1 
 
     return (counter/len(grades))
 
-def calculate_average(grades)
+def calculate_average(grades):
     if len(grades) == 0:
         return 0
 
     avg = sum(grades)/len(grades)
     return avg
 
-def kickstart()
-    infile = open("final.txt", "r")
-    grades = [word.strip() for word infile]
-    infile.close()
-
 
 main()
-
-
-
-"""
