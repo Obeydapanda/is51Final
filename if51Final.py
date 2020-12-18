018a5b822d2a23b9aefbb23919cd492055bbb2c1
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-12-17 18:08:59 -0800

    Psuedo_Code Finished

--- if51Final.py
+++ if51Final.py
@@ -1,9 +1,38 @@
 """
-This program will put the final.txt into an int string
-assign each number to a list
-calculate the average for the grade
-then see how many grades were above the average
-print out values for a count of the grades, average, and people above average
+def main():
+    
+    kickstart()
+    calculate_average()
+    calculate_percent_above_avereage()
+
+    print("Number of Grades:", len(grades))
+    print("Average Grade:", calculate_average(grades))
+    print("Percentage of Grades Above Average:", calculate_percent_above_avereage(grades), "%")
+
+def calculate_percent_above_avereage(grades):
+    avg = calculate_average(grades)
+    counter = 0
+    for grades in grades:
+        if grade > avg:
+            counter += 1 
+
+    return (counter/len(grades))
+
+def calculate_average(grades)
+    if len(grades) == 0:
+        return 0
+
+    avg = sum(grades)/len(grades)
+    return avg
+
+def kickstart()
+    infile = open("final.txt", "r")
+    grades = [word.strip() for word infile]
+    infile.close()
+
+
+main()
+
 
 
 """
