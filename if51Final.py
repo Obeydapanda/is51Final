11dbd5066bb3fd9eeb2ec03ce90557973638a1e9
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-12-17 17:59:03 -0800

    Structured English Finished

---
+++ if51Final.py
@@ -0,0 +1,9 @@
+"""
+This program will put the final.txt into an int string
+assign each number to a list
+calculate the average for the grade
+then see how many grades were above the average
+print out values for a count of the grades, average, and people above average
+
+
+"""
